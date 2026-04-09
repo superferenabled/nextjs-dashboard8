972fdae9617028940e130a28a434b3886f20cdbc
Fernando Castillo <superferenabled@gmail.com>
2026-04-08 23:35:17 -0600

    Enhance PokemonPage layout with detailed information and images; update next.config for additional image sources

--- app/dashboard/pokemon/[id]/page.tsx
+++ app/dashboard/pokemon/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { Pokemon } from "@/app/pokemons";
 import { use } from "react";
+import Image from "next/image";
 
 interface Params {
-  id: string
+  id: string;
 }
 
 // interface SearchParams {
@@ -16,22 +17,19 @@ interface RouteProps {
   // searchParams: Promise<SearchParams>;
 }
 
-export const generateMetadata = async ({params}: RouteProps) => {
+export const generateMetadata = async ({ params }: RouteProps) => {
   const { id } = await params;
-  const {id: pokeId, name} = await getPokemon(id);
+  const { id: pokeId, name } = await getPokemon(id);
   return {
     title: `#${pokeId} - ${name}`,
-    description: `Página del pakimon ${name}`
-  }
-}
+    description: `Página del pakimon ${name}`,
+  };
+};
 
-const getPokemon = async (
-  id: string
-) : Promise<Pokemon> => {
-  const pokemon = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${id}`,
-    {cache: "force-cache"}
-  ).then((res) => res.json());
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+    cache: "force-cache",
+  }).then((res) => res.json());
   console.log(`Se cargó: ${pokemon.name}`);
   return pokemon;
 };
@@ -41,15 +39,91 @@ export const PokemonPage = ({ params }: RouteProps) => {
   const { id } = use(params);
   // const { a, b, c } = use(searchParams);
   const pokemon = use(getPokemon(id));
-  
+
   return (
-    <>
-      <div>Pokemon id: {id}, </div>
-      <div>{pokemon.name}</div>
-      {/* { a && <div>a: {a}</div> }
-      { b && <div>b: {b}</div> }
-      { c && <div>c: {c}</div> } */}
-    </>
+    <div className="flex mt-5 flex-col items-center text-slate-800">
+      <div className="relative flex flex-col items-center rounded-[20px] w-[700px] mx-auto bg-white bg-clip-border  shadow-lg  p-3">
+        <div className="mt-2 mb-8 w-full">
+          <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
+            #{pokemon.id} {pokemon.name}
+          </h1>
+          <div className="flex flex-col justify-center items-center">
+            <Image
+              src={pokemon.sprites.other?.dream_world.front_default ?? ""}
+              width={150}
+              height={150}
+              alt={`Imagen del pokemon ${pokemon.name}`}
+              className="mb-5"
+            />
+
+            <div className="flex flex-wrap">
+              {pokemon.moves.map((move) => (
+                <p key={move.move.name} className="mr-2 capitalize">
+                  {move.move.name}
+                </p>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 px-2 w-full">
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
+            <p className="text-sm text-gray-600">Types</p>
+            <div className="text-base font-medium text-navy-700 flex">
+              {pokemon.types.map((type) => (
+                <p key={type.slot} className="mr-2 capitalize">
+                  {type.type.name}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
+            <p className="text-sm text-gray-600">Peso</p>
+            <span className="text-base font-medium text-navy-700 flex">
+              {pokemon.weight}
+            </span>
+          </div>
+
+          <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
+            <p className="text-sm text-gray-600">Regular Sprites</p>
+            <div className="flex justify-center">
+              <Image
+                src={pokemon.sprites.front_default}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+
+              <Image
+                src={pokemon.sprites.back_default}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
+            <p className="text-sm text-gray-600">Shiny Sprites</p>
+            <div className="flex justify-center">
+              <Image
+                src={pokemon.sprites.front_shiny}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+
+              <Image
+                src={pokemon.sprites.back_shiny}
+                width={100}
+                height={100}
+                alt={`sprite ${pokemon.name}`}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
